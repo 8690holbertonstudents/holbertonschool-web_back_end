cbf2f33b857c1a955e09e17bafc0bd189242825a
8690holbertonstudents <8690@holbertonstudents.com>
2024-07-18 14:46:43 +0200

    ES6_classes - T2 (corr 1)

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,31 +1,40 @@
 export default class HolbertonCourse {
-  constructor(name = '', lenght = 0, students = []) {
+  constructor(name, lenght, students) {
     this._name = name;
     this._lenght = lenght;
     this._students = students;
-  }
-
-  set name(newName) {
-    this._name = newName;
-  }
-
-  set lenght(newLenght) {
-    this._lenght = newLenght;
-  }
-
-  set students(newStudents) {
-    this._students = newStudents;
   }
 
   get name() {
     return this._name;
   }
 
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
   get lenght() {
     return this._lenght;
   }
 
+  set lenght(newLenght) {
+    if (typeof newLenght !== 'number') {
+      throw new TypeError('Lenght must be a number');
+    }
+    this._lenght = newLenght;
+  }
+
   get students() {
     return this._students;
+  }
+
+  set students(newStudents) {
+    if (typeof Array.isArray(newStudents) === 'false') {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStudents;
   }
 }
