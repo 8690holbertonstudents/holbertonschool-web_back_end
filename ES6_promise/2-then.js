3fadac6921973d7b4d7d4e074fa3cf878e86e61a
8690holbertonstudents <8690@holbertonstudents.com>
2024-07-23 19:35:28 +0200

    ES6_promises - T2 (corr 1)

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   const LogMsg = 'Got a response from the API';
   promise.then(() => {
-    return console.log(LogMsg);
-  });
-  promise.then((resolve) => {
     return { status: 200, body: 'Success' };
   });
-  promise.then((reject) => {
+  promise.catch(() => {
     return new Error();
+  });
+  promise.finally(() => {
+    return console.log(LogMsg);
   });
 }
