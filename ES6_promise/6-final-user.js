09035d142649a4d284d2c3b679659683be5097f9
8690holbertonstudents <8690@holbertonstudents.com>
2024-07-26 12:06:00 +0200

    ES6_promise - T6 (corr 1)

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,25 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  
-  if (
-    typeof firstName !== 'string' ||
-    typeof lastName !== 'string' ||
-    typeof fileName !== 'string'
-  ) {
-    throw new Error('All arguments must be strings');
-  }
-  
-  const allPromises = Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
-
-  Promise.allSettled(allPromises).then((results) => {
-    const outputResults = [];
-    results.forEach((result) => {
-      outputResults.push({ status: result.status, value: result.value });
-    });
+  ]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.value,
+    }));
   });
-  return outputResults;
 }
