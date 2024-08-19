8ad5668a3e4e08c0191594505bad219a40b01708
8690holbertonstudents <8690@holbertonstudents.com>
2024-08-19 12:10:12 +0200

    Python - Async Comprehension - T2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension():
     """
+    Function to use async generator function
     """
     return [num async for num in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module to use Python - Async
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Function to measure runtime of async comprehension (that call async generator)
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for num in range(4)))
+    end_time = time.time()
+    return end_time - start_time
