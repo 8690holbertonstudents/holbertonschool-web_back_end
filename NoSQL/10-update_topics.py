ee8f37dac56a32a50aa74306accfaf841ba105ca
8690holbertonstudents <8690@holbertonstudents.com>
2024-08-23 11:28:17 +0200

    NoSQL - T12

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,7 +6,8 @@ Module to use MongoDB with python
 
 def update_topics(mongo_collection, name, topics):
     """
-    Function that that changes all topics of a school document based on the name
+    Function that that changes all topics
+    of a school document based on the name
     """
     update_doc = mongo_collection.update_many(
         {"name": name}, {"$set": {"topics": topics}})
---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Python script that provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    db = client.logs
+    collection = db.nginx
+
+    print(f"{collection.count_documents({})} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("Methods:")
+
+    for method in methods:
+        m_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {m_count}")
+
+    s_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{s_count} status check")
